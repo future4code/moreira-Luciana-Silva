6a3397d27e51ab584b04fea048f2a5a389101c64
LUCIANA LOPES <luciana.lopes.moc@gmail.com>
2022-01-13 17:52:48 -0300

    Inclui o exerc renderização de listas

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -8,19 +8,48 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
-
 class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'Roberta'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-      </MainContainer>
-    );
-  }
-}
 
+  state ={
+    listaDePosts:[ {
+      nomeUsuario:'Paulinha',
+      fotoUsuario:'https://picsum.photos/50/50',
+      fotoPost:'https://picsum.photos/200/150'
+    },    
+
+    {
+     nomeUsuario:'Angelica',
+      fotoUsuario:'https://picsum.photos/51/51',
+      fotoPost:'https://picsum.photos/200/160'
+     },
+  
+     {
+      nomeUsuario:'Maria',
+      fotoUsuario:'https://picsum.photos/54/54',
+      fotoPost:'https://picsum.photos/200/170'
+  }]
+
+  }
+
+  render() {
+
+  const listaDePosts = this.state.listaDePosts.map((post, index)=>{
+      
+    return <Post key ={index}
+    nomeUsuario = {post.nomeUsuario}
+    fotoUsuario = {post.fotoUsuario}
+    fotoPost = {post.fotoPost}  
+      />
+  })
+
+   return (
+     <MainContainer>
+        {listaDePosts}
+     </MainContainer>
+
+ );
+   }
+  }     
+ 
+                      
 export default App;
